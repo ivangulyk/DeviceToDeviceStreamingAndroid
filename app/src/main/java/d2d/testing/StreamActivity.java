fa3708c70c268f4a3f0e23e7c7d011779846681f
Noel Algora <noelalgora@lignumcap.com>
2019-01-14 22:57:35 +0100

    StreamActivity RTSP server

---
+++ app/src/main/java/d2d/testing/StreamActivity.java
@@ -0,0 +1,47 @@
+package d2d.testing;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.ActivityInfo;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+import d2d.testing.streaming.Session;
+import d2d.testing.streaming.SessionBuilder;
+import d2d.testing.streaming.gl.SurfaceView;
+import d2d.testing.streaming.rtsp.RtspServer;
+
+public class StreamActivity extends AppCompatActivity {
+
+    private final static String TAG = "MainActivity";
+
+    private SurfaceView mSurfaceView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        setContentView(R.layout.activity_stream);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        mSurfaceView = findViewById(R.id.surface);
+
+        // Sets the port of the RTSP server to 1234
+        SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(this).edit();
+        editor.putString(RtspServer.KEY_PORT, String.valueOf(1234));
+        editor.commit();
+
+        // Configures the SessionBuilder
+        SessionBuilder.getInstance()
+                .setSurfaceView(mSurfaceView)
+                .setPreviewOrientation(90)
+                .setContext(getApplicationContext())
+                .setAudioEncoder(SessionBuilder.AUDIO_AAC)
+                .setVideoEncoder(SessionBuilder.VIDEO_H264);
+
+        // Starts the RTSP server
+        this.startService(new Intent(this,RtspServer.class));
+    }
+}
